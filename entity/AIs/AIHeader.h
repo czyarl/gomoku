0818f81ba5058060a9b563a6468162540c2abd1e
czyarl <56596386+czyarl@users.noreply.github.com>
2021-06-11 11:59:56 +0800

    Create AIHeader.h

---
+++ entity/AIs/AIHeader.h
@@ -0,0 +1,18 @@
+#ifndef AI_HEADER_H
+#define AI_HEADER_H
+
+#ifndef LONG_LONG_MAX//in case linux does not have it
+#define LONG_LONG_MAX 9223372036854775807//2^63 - 1
+#endif
+
+#include <utility>
+#include <cstdio>
+#include <stdlib.h>
+#include <algorithm>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "../../basic/BasicDef.h"
+#include "../EntityControl.h"
+
+#endif
